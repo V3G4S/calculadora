76da1d18bcfd3bb62b808589eeb409c540e1037c
ANTONY VALENTIM GARCIA DOS SANTOS <antony_v_santos@estudante.sesisenai.org.br>
2024-09-20 08:58:21 -0300

    updRaizPorcent

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-function insert(num){
-    var numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = numero + num;
+function insert(num) {
+    var numero = document.getElementById('resultado').innerHTML
+    document.getElementById('resultado').innerHTML = numero + num
 }
-function clean(){
-    document.getElementById('resultado').innerHTML = "";
+
+function clean() {
+    document.getElementById('resultado').innerHTML = ""
 }
-function back(){
-    var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+
+function back() {
+    var resultado = document.getElementById('resultado').innerHTML
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1)
 }
-function calcular(){
-    var resultado = document.getElementById('resultado').innerHTML;
-    if(resultado){
-        document.getElementById('resultado').innerHTML = eval(resultado)
-    }else{
-        document.getElementById('resultado').innerHTML = "Não há valores"
+
+function calcular() {
+    var resultado = document.getElementById('resultado').innerHTML
+    if (resultado.includes('%')) {
+        var partes = resultado.split('%')
+        var num = eval(partes[0])
+        document.getElementById('resultado').innerHTML = (num / 100) * (partes[1] ? eval(partes[1]) : 1)
+    } else {
+        try {
+            document.getElementById('resultado').innerHTML = eval(resultado)
+        } catch (e) {
+            document.getElementById('resultado').innerHTML = "Erro"
+        }
     }
 }
-function porcento(){
-    var resultado = document.getElementById('resultado').innerHTML = eval(resultado)
+
+function porcento() {
+    var resultado = document.getElementById('resultado').innerHTML
+    if (resultado) {
+        var num = eval(resultado)
+        document.getElementById('resultado').innerHTML = num / 100
+    }
+}
+
+function raiz() {
+    var resultado = document.getElementById('resultado').innerHTML
+    if (resultado) {
+        var num = eval(resultado)
+        document.getElementById('resultado').innerHTML = Math.sqrt(num)
+    }
 }
